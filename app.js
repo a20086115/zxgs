2dbd65036cd02581d0e8a3eeb7e25932239a58bd
于文强 <ywq_neu@163.com>
2019-04-21 19:40:18 +0800

    http and https

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var http = require('http');
 var https = require('https');
+
 var privateKey  = fs.readFileSync('keys/my.key', 'utf8');
 var certificate = fs.readFileSync('keys/my.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
-var app = express();
-
-var httpsServer = https.createServer(credentials, app);
+var PORT = 80;
 var SSLPORT = 443;
 
-httpsServer.listen(SSLPORT, function() {
-    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
-});
+var app = express();
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
 app.use(express.static(path.join(__dirname, 'static')));
+
+httpServer.listen(PORT, function() {
+  console.log('HTTP Server is running on: http://www.jiejiangzs.com/');
+});
+
+httpsServer.listen(SSLPORT, function() {
+  console.log('HTTPS Server is running on: https://www.jiejiangzs.com/');
+});
 
 // var express = require('express');
 // var path = require('path');
