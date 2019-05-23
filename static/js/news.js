c57528926cecf7a716dcc70afb9b245032934383
于文强 <ywq_neu@163.com>
2019-05-24 00:32:46 +0800

    移到云

---
+++ static/js/news.js
@@ -0,0 +1,46 @@
+var arrs = [
+    {
+        text: "办公室装修拆改需要注意哪些问题？",
+        content: `
+        <p>
+        <span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">&nbsp;&nbsp;&nbsp;&nbsp;在</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">广西办公室</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">装修、</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">南宁办公室</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">装修、</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">南宁</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">写字楼重新装修</span>……中，要数</span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">南宁</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">办公室装修是最常见的。新装修，如果装修处理不当的话，会带来很大的麻烦。如在办公室装修中拆墙这个环节就有诸多禁忌。</span></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; <strong>1、承重梁柱不可拆</strong></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp;梁柱是用来支撑上层楼板的，拆掉后上层楼板就会掉下来，后果非常严重，所以也不能随意拆改破坏。</span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; <strong>2、承重墙不可拆</strong></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; 凡是预制板墙一律不能拆除或开门开窗；超过24厘米以上的砖墙也属于承重墙，也是不能拆改的。</span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; <strong>3、阳台边的矮墙不可拆</strong></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; 门窗都可以拆改，但窗以下的墙却不能动。这段墙叫“配重墙”，它像秤砣一样起着挑起阳台的作用。拆改这堵墙，导致阳台的承重力下降，导致阳台下坠，造成不可预计的后果。</span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; <strong>4、卫生间和防水层</strong></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)">&nbsp; 这些地方的地面下都有防水层，如果破坏了，楼下就会变成“水帘洞”。所以在您更换地面材料时，一定注意不要破坏防水层。如果破坏后重新修建，一定要重新做防水工程，经过“24小时渗水实验”后，如果不漏水，才算合格。</span><span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><br/></span>
+    </p>
+    <p>
+        <br/>
+    </p>   
+    `},
+    {
+        text: "办公室装修设计基本原则",
+        content: `
+        <p style="text-align: center;">
+        <span style="font-family: 微软雅黑;color: rgb(51, 51, 51);letter-spacing: 0;font-size: 19px;background: rgb(255, 255, 255)"><span style="font-family:微软雅黑">办公室装修设计基本原则</span></span>
+    </p>
+    <p>
+        <span style="color: rgb(48, 48, 48); letter-spacing: 0px; font-size: 16px; background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial; font-family: 宋体;">&nbsp; &nbsp;俗话说：无规矩不一成方圆。</span><a href="http://www.dghtzs168.com/"><span style="color: rgb(31, 26, 22); letter-spacing: 0px; font-size: 16px; font-family: 宋体;">办公室装修</span></a><span style="color: rgb(48, 48, 48); letter-spacing: 0px; font-size: 16px; background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial; font-family: 宋体;">相对于普通写字楼办公室装修既有他的相同之处，又有他自身独特的特点。那么</span><a href="http://www.dghtzs168.com/"><span style="color: rgb(31, 26, 22); letter-spacing: 0px; font-size: 16px; font-family: 宋体;">办公室装修设计</span></a><span style="color: rgb(48, 48, 48); letter-spacing: 0px; font-size: 16px; background-image: initial; background-position: initial; background-size: initial; background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial; font-family: 宋体;">都有哪些原则呢？今天杰匠装饰的小编为您一一讲解。</span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 0;padding: 0;background: rgb(255, 255, 255)">
+        <strong><span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 11px"><span style="font-family:宋体">办公室设计有三个层次的目标</span></span></strong><span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 11px"><span style="font-family:宋体">：</span></span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 0;padding: 0;background: rgb(255, 255, 255)">
+        <span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 11px">　</span><span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 16px"><span style="font-family:宋体">　 第一层次是经济实用，一方面要满足实用要求、给办公人员的工作带来方便，另一方面要尽量低费用、追求最佳的功能费用比</span>;</span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 0;padding: 0;background: rgb(255, 255, 255)">
+        <span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 16px"><span style="font-family:宋体">　　第二层次是美观大方，能够充分满足人的生理和心理需要，创造出一个赏心悦目的良好工作环境</span>;</span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 0;padding: 0;background: rgb(255, 255, 255)">
+        <span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 16px"><span style="font-family:宋体">　　第三层次是独具品味，办公室是企业文化的物质载体，要努力体现企业物质文化和精神文化，反映企业的特色和形象，对置身其中的工作人员产生积极的、和谐的影响。</span></span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 0;padding: 0;background: rgb(255, 255, 255)">
+        <span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 16px"><span style="font-family:宋体">　　这三个层次的目标虽然由低到高、由易到难，但它们不是孤立的、而是有着紧密的内在联系，出色的办公室设计应该努力同时实现这三个目标。</span></span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 0;padding: 0;background: rgb(255, 255, 255)">
+        <span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 16px"><span style="font-family:宋体">　　办公室装修最重要的就是风格选择，一般流行三种风格。分别为：稳重凝炼型、现代型、普遍适用型。所以办公室装修特别强调功能和空间的利用，必须让空间发挥出最大的使用率，且办公室一定要体现出公司的独特文化。</span></span>
+    </p>
+    <p style="margin: 0 0 8px;text-indent: 32px;padding: 0;background: rgb(255, 255, 255)">
+        <span style="font-family: 宋体;color: rgb(48, 48, 48);letter-spacing: 0;font-size: 16px"><span style="font-family:宋体">办公室装修的定义是：即处理一种特定事务的地方或提供服务的地方，而办公室装修则能恰到好处的突出公司、企业的内部文化，同时办公室的装修风格也能彰显出其使用者的性格特征，办公室装修的好坏直接影响整个企业、公司的形象，所以随着科技水平的提高，对于办公室装修的要求也不再只是单纯的独立一个空间给个人使用，更多是要体现出简约、时尚、舒适、实用的感受，让身在其中的人有积极向上的的生活、工作追求。</span></span>
+    </p>
+    <p>
+        <br/>
+    </p> 
+    `},
+]
