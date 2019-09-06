7607bc522423fa953a1078692da8049d23239687
Yu wenqiang <yuwenqiang@topscomm.com>
2019-09-06 16:29:16 +0800

    cdn加速域名

--- static/js/shigong.js
+++ static/js/shigong.js
@@ -1,1 +1,1 @@
-var shigong = [{"text":"施工现场的标题","desc":"施工现场在列表处的简介","date":"2019-04-01","content":"施工现场内容","image":"https://jiejiangs-1253930943.cos.ap-guangzhou.myqcloud.com/demoImages/KTV/图片8.jpg"}]
+var shigong = [{"text":"施工现场的标题","desc":"施工现场在列表处的简介","date":"2019-04-01","content":"施工现场内容","image":"https://jiejiangs-1253930943.file.myqcloud.com/demoImages/KTV/图片8.jpg"}]
